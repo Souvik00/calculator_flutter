13e1d4d4f4b6911f7e24e5e03365060bb07f27cc
souvik <souvikavro00.sa@gmail.com>
2023-08-14 00:01:13 +0600

    basic edited

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,7 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
@@ -11,114 +10,60 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        backgroundColor: Colors.teal.shade900,
-        appBar: AppBar(
-          backgroundColor: Colors.teal.shade100,
-          centerTitle: true,
-          title: Text(
-            'Calculator',
-            style: TextStyle(
-              fontFamily: 'Coutgette',
-              fontSize: 30.0,
-              color: Colors.black,
+      title: 'Flutter Demo',
+      theme: ThemeData(
+
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+
+  void _incrementCounter() {
+    setState(() {
+      _counter++;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const Text(
+              'You have pushed the button this many times:',
             ),
-          ),
-        ),
-        body: SafeArea(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Container(
-                margin: EdgeInsets.all(40.0),
-                color: Colors.white,
-                width: 300.0,
-                child: TextField(
-                  decoration: InputDecoration(
-                    border: OutlineInputBorder(),
-                    hintText: 'Enter values',
-
-                    hintStyle: TextStyle(
-
-                      fontSize: 30.0,
-                      //fontWeight: FontWeight.bold,
-                    ),
-                  ),
-                ),
-              ),
-
-              Column(
-
-                children: [
-
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Card(
-                        //margin: EdgeInsets.,
-                        color: Colors.white,
-                        child: Icon(
-                          CupertinoIcons.plus,
-                          color: Colors.black,
-                          size: 100.0,
-                        ),
-                      ),
-                      Card(
-                        color: Colors.white,
-                        child: Icon(
-                          CupertinoIcons.minus,
-                          color: Colors.black,
-                          size: 100.0,
-                        ),
-                      ),
-                    ],
-                  ),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Card(
-                        color: Colors.white,
-                        child: Icon(
-                          CupertinoIcons.multiply,
-                          color: Colors.black,
-                          size: 100.0,
-                        ),
-                      ),
-                      Card(
-                        color: Colors.white,
-                        child: Icon(
-                          CupertinoIcons.divide,
-                          color: Colors.black,
-                          size: 100.0,
-                        ),
-                      ),
-                    ],
-                  ),
-                ],
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Card(
-                    margin:
-                    EdgeInsets.symmetric(vertical: 5.0, horizontal: 5.0),
-                    color: Colors.yellow.shade600,
-                    child: Padding(
-                      padding: const EdgeInsets.symmetric(
-                          vertical: 0.0, horizontal: 55.0),
-                      child: Icon(
-                        CupertinoIcons.equal,
-                        color: Colors.black,
-                        size: 100.0,
-                      ),
-                    ),
-                  ),
-                ],
-              ),
-            ],
-          ),
+            Text(
+              '$_counter',
+              style: Theme.of(context).textTheme.headlineMedium,
+            ),
+          ],
         ),
       ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _incrementCounter,
+        tooltip: 'Increment',
+        child: const Icon(Icons.add),
+      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
